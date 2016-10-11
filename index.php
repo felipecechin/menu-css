68d5116de93269bd168e721e9d6beeb7ca73ec09
alunos <alunos@172.17.10.159>
2016-10-11 09:06:36 -0300

    Arquivos de base do sistema criados.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="estilos.css"/>
+        <title></title>
+    </head>
+    <body>
+
+        <ul>
+            <li>
+                <a href="#">Menu 1</a>
+                <ul>
+                    <li> <a href="#">Submenu 1</a></li>
+                    <li> <a href="#">Submenu 2</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Menu 2</a></li>
+            <li><a href="#">Menu 3</a>
+                <ul>
+                    <li><a href="#"> Submenu 3</a></li>
+                </ul>
+            </li>
+        </ul>
+    </body>
+</html>
